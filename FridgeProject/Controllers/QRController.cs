f53a00f11557f2e94d8a6e17b729aa642b96bebe
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-11-02 19:37:22 +0200

    Add QRController

---
+++ FridgeProject/Controllers/QRController.cs
@@ -0,0 +1,42 @@
+﻿using FridgeProject.Models.QR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateQRCode(QRCodeModel qRCode)
+        {
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(qRCode.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            return Ok(QrUri);
+        }
+    }
+    public static class BitmapExtension
+    {
+        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
