9bad9cdfaf97f7a96cfadba3e7235522e4f61a5b
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-11-05 23:50:21 +0200

    Add actions

---
+++ FridgeProject/ClientApp/src/actions/fridge.js
@@ -0,0 +1,170 @@
+﻿import EventBus from "../common/EventBus";
+import { CREATE_FRIDGE_ERROR, CREATE_FRIDGE_SUCCESS, DELETE_FRIDGE_ERROR, DELETE_FRIDGE_SUCCESS, EDIT_FRIDGE_ERROR, EDIT_FRIDGE_SUCCESS, GET_FRIDGE_ERROR, GET_FRIDGE_SUCCESS, GET_OWN_FRIDGES, GET_SHARED_FRIDGES } from "../constants/fridge";
+import { SET_MESSAGE } from "../constants/message";
+import fridgeService from "../services/fridge.service";
+
+export const getOwnFridges = () => (dispatch) => {
+    return fridgeService.getOwnFridges().then(
+        (responce) => {
+            dispatch({
+                type: GET_OWN_FRIDGES,
+                payload: { ownFridges: responce.data }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            return Promise.reject();
+        }
+    )
+}
+
+export const getSharedFridges = () => (dispatch) => {
+    return fridgeService.getSharedFridges().then(
+        (responce) => {
+            dispatch({
+                type: GET_SHARED_FRIDGES,
+                payload: { sharedFridges: responce.data }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            return Promise.reject();
+        }
+    )
+}
+
+export const getFridge = (id) => (dispatch) => {
+    return fridgeService.getFridge(id).then(
+        (responce) => {
+            dispatch({
+                type: GET_FRIDGE_SUCCESS,
+                payload: { fridge: responce.data }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            dispatch({
+                type: GET_FRIDGE_ERROR
+            });
+
+            return Promise.reject();
+        }
+    )
+}
+
+export const editFridge = (fridgeId, fridgeName) => (dispatch) => {
+    return fridgeService.editFridge(fridgeId, fridgeName).then(
+        (responce) => {
+            dispatch({
+                type: EDIT_FRIDGE_SUCCESS,
+                payload: { fridgeId, fridgeName }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            dispatch({
+                type: EDIT_FRIDGE_ERROR
+            });
+
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.title) || error.response.data || error.response.data.title;
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message,
+            });
+
+            return Promise.reject();
+        }
+    )
+}
+
+export const createFridge = (fridgeName, userId) => (dispatch) => {
+    return fridgeService.createFridge(fridgeName, userId).then(
+        (responce) => {
+            dispatch({
+                type: CREATE_FRIDGE_SUCCESS,
+                payload: { fridge: responce.data }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            dispatch({
+                type: CREATE_FRIDGE_ERROR
+            });
+
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.title) || error.response.data || error.response.data.title;
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message,
+            });
+
+            return Promise.reject();
+        }
+    )
+}
+
+export const deleteFridge = (id) => (dispatch) => {
+    return fridgeService.deleteFridge(id).then(
+        (responce) => {
+            dispatch({
+                type: DELETE_FRIDGE_SUCCESS,
+                payload: { fridgeId: id }
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+
+            dispatch({
+                type: DELETE_FRIDGE_ERROR
+            });
+
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.title) || error.response.data || error.response.data.title;
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message,
+            });
+
+            return Promise.reject();
+        }
+    )
+}
---
+++ FridgeProject/ClientApp/src/services/fridge.service.js
@@ -0,0 +1,32 @@
+﻿import axios from "axios";
+import { CURRENT_DOMAIN } from "../utils/domain";
+
+const API_URL = CURRENT_DOMAIN + "/Fridges/";
+
+class FridgeService {
+    getOwnFridges() {
+        return axios.get(API_URL + "own/all", { headers: authHeader() });
+    }
+
+    getSharedFridges() {
+        return axios.get(API_URL + "shared/all", { headers: authHeader() });
+    }
+
+    getFridge(id) {
+        return axios.get(API_URL + "one/" + id, { headers: authHeader() });
+    }
+
+    editFridge(fridgeId, fridgeName) {
+        return axios.put(API_URL + "edit", { fridgeId, fridgeName }, { headers: authHeader() });
+    }
+
+    createFridge(fridgeName, userId) {
+        return axios.post(API_URL + "create", { fridgeName, userId }, { headers: authHeader() });
+    }
+
+    deleteFridge(id) {
+        return axios.post(API_URL + "delete/" + id, { headers: authHeader() });
+    }
+}
+
+export default new FridgeService();
